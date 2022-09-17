9b08a520e7e10e9a29b3422d181b2469ea3d3510
Alex Mathew <alexmathew003@gmail.com>
2022-09-17 21:25:51 +0530

    Implement layout changes for the result prompt section

--- src/components/FinalPrompt.tsx
+++ src/components/FinalPrompt.tsx
@@ -11,6 +11,21 @@ const FinalPrompt: React.FC = () => {
     .filter((option) => !!option)
     .join(", ");
 
+  const finalText = `${prompt}${
+    parametersText ? `${prompt ? ", " : ""}${parametersText}` : ""
+  }`;
+
+  const copyPrompt = () => {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(finalText).then(
+        () => {},
+        (err) => {
+          console.log("Failed to copy the text to clipboard.", err);
+        }
+      );
+    }
+  };
+
   return (
     <div
       className="h-screen w-full grid justify-items-center content-center"
@@ -18,9 +33,18 @@ const FinalPrompt: React.FC = () => {
         background: "linear-gradient(135deg, #FFD3A5 0%, #FD6585 100%)",
       }}
     >
-      <div className="text-3xl font-bold w-2/3">
-        {prompt}
-        {parametersText ? `${prompt ? "," : ""} ${parametersText}` : ""}
+      <div className="w-4/5 h-[80vh] bg-white grid grid-rows-4 justify-items-center content-center rounded-lg shadow-lg">
+        <div className="row-span-3 self-center w-3/4">
+          <div className="text-3xl font-normal leading-[60px]">{finalText}</div>
+        </div>
+        <div className="row-span-1 self-center">
+          <button
+            className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-[20vh]"
+            onClick={copyPrompt}
+          >
+            Copy To Clipboard
+          </button>
+        </div>
       </div>
     </div>
   );
