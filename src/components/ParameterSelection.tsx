818a26ffd2ec54f3a331211493f8eaa575e0c7d4
Alex Mathew <alexmathew003@gmail.com>
2022-09-23 23:02:45 +0530

    Implement see more/see less for options

--- src/components/ParameterSelection.tsx
+++ src/components/ParameterSelection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCurrentUser } from "../context/CurrentUserContext";
 import {
   useParameterSelection,
@@ -8,9 +8,43 @@ import {
 interface ParameterSelectionProps {}
 
 const ParameterSelection: React.FC<ParameterSelectionProps> = () => {
+  const [seeMoreOfParameter, setSeeMoreOfParameter] = useState<{
+    [parameter: string]: boolean;
+  }>({});
   const promptParameters = useParameterSelection();
   const setPromptParameter = useParameterSelectionUpdate();
   const currentUser = useCurrentUser();
+
+  const getParameterOptionsDiv = (
+    range: string[],
+    parameter: string,
+    index: number
+  ) => {
+    console.log(range);
+
+    return (
+      <>
+        {range.map((option: string, optionIndex: number) => {
+          const isSelected = promptParameters[parameter] === option;
+          return (
+            <div
+              key={`${index}_${optionIndex}`}
+              className={`cursor-pointer ${
+                isSelected ? "font-bold" : "font-normal"
+              } ${isSelected ? "text-black" : "text-gray-400"}`}
+              onClick={(e) => {
+                setPromptParameter({
+                  [parameter]: isSelected ? "" : option,
+                });
+              }}
+            >
+              {(currentUser.parameters || {})[parameter][option]}
+            </div>
+          );
+        })}
+      </>
+    );
+  };
 
   return (
     <>
@@ -23,26 +57,51 @@ const ParameterSelection: React.FC<ParameterSelectionProps> = () => {
               {parameter}
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 gap-x-4 gap-y-2">
-              {Object.keys((currentUser.parameters || {})[parameter]).map(
-                (option: string, optionIndex: number) => {
-                  const isSelected = promptParameters[parameter] === option;
-                  return (
-                    <div
-                      key={`${index}_${optionIndex}`}
-                      className={`cursor-pointer ${
-                        isSelected ? "font-bold" : "font-normal"
-                      } ${isSelected ? "text-black" : "text-gray-400"}`}
-                      onClick={(e) => {
-                        setPromptParameter({
-                          [parameter]: isSelected ? "" : option,
-                        });
-                      }}
-                    >
-                      {(currentUser.parameters || {})[parameter][option]}
-                    </div>
-                  );
-                }
+              {getParameterOptionsDiv(
+                Object.keys((currentUser.parameters || {})?.[parameter]).slice(
+                  0,
+                  9
+                ),
+                parameter,
+                index
               )}
+              {Object.keys((currentUser.parameters || {})?.[parameter]).length >
+              9 ? (
+                seeMoreOfParameter?.[parameter] ? (
+                  getParameterOptionsDiv(
+                    Object.keys(
+                      (currentUser.parameters || {})?.[parameter]
+                    ).slice(9),
+                    parameter,
+                    index
+                  )
+                ) : (
+                  <div
+                    className={`cursor-pointer font-normal text-[#FD6585]`}
+                    onClick={(e) => {
+                      setSeeMoreOfParameter({
+                        ...seeMoreOfParameter,
+                        [parameter]: true,
+                      });
+                    }}
+                  >
+                    See More
+                  </div>
+                )
+              ) : null}
+              {seeMoreOfParameter?.[parameter] ? (
+                <div
+                  className={`cursor-pointer font-normal text-[#FD6585]`}
+                  onClick={(e) => {
+                    setSeeMoreOfParameter({
+                      ...seeMoreOfParameter,
+                      [parameter]: false,
+                    });
+                  }}
+                >
+                  See Less
+                </div>
+              ) : null}
             </div>
           </div>
         )
