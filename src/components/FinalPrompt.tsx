04719f67599eb32357f020d53ab5fd534ce27dc0
Alex Mathew <alexmathew003@gmail.com>
2022-09-27 23:37:46 +0530

    Manage overflow in final prompt for large prompts

--- src/components/FinalPrompt.tsx
+++ src/components/FinalPrompt.tsx
@@ -105,12 +105,12 @@ const FinalPrompt: React.FC = () => {
         )}
       </div>
       <div className="w-4/5 h-[80vh] bg-white grid grid-rows-4 justify-items-center content-center rounded-lg shadow-lg">
-        <div className="row-span-3 self-center w-3/4">
+        <div className="row-span-3 self-center w-3/4 max-h-full overflow-y-scroll">
           <div className="text-3xl font-normal leading-[60px]">{finalText}</div>
         </div>
         <div className="row-span-1 self-center">
           <button
-            className=" bg-[#FD6585] text-white font-semibold py-2 px-4 rounded my-[20vh]"
+            className=" bg-[#FD6585] text-white font-semibold py-2 px-4 rounded"
             onClick={() => {
               copyPrompt();
               setCopied(true);
